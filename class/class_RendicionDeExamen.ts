1107789b8eec0bd6a362c608226b485cb1c73c5f
A90G <104040612+A90G@users.noreply.github.com>
2022-10-24 20:20:35 -0300

    Update class_RendicionDeExamen.ts
    falta revisión método estaAprobado()

--- class/class_RendicionDeExamen.ts
+++ class/class_RendicionDeExamen.ts
@@ -8,14 +8,23 @@ export class RendicionDeExamen {
         this._examen = p_examen;
         this._respuestas = p_respuestas;
     }
-    public rendicionDeExamen(p_examen: Examen,p_respuestas:Array<number>){
+    public rendicionDeExamen(p_examen: Examen, p_respuestas: Array<number>){
 
     }
-    public getExamen():Examen{
+    public getExamen(): Examen{
         return this._examen;
     }
-    public estaAprobado ():boolean{
-        return false
-    }
+    public estaAprobado(): boolean{
+        /* cómo traigo los valores??
+        let values = [2, 56, 3, 41, 0, 4, 100, 23];
+        let sum = values.reduce((previous, current) => current += previous);
+        let _promedio = sum / values.length;
+*/
+        if (_promedio  > 7) {
+         return true;
+      } else {
+         return false;
+      }
+      }
 
 }
