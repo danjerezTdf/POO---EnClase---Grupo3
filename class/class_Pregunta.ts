1a029753217cab495de2676525f2927cf3f8e7f4
Your Name <dgj7785@gmail.com>
2022-10-24 20:02:44 -0300

    upgrade_class_Alumno_V1

--- class/class_Pregunta.ts
+++ class/class_Pregunta.ts
@@ -10,7 +10,9 @@ constructor (p_consigna:string, p_opciones:Array<string>,p_opcionCorrecta:number
     this._puntaje = p_puntaje;
 }
  public pregunta(p_consigna:string, p_puntaje:number){
-
+    this._consigna = p_consigna;
+    this._puntaje = p_puntaje;
+    console.log("pregunta: "+this._consigna+" Con Puntaje de: "+this._puntaje)
  }
  public addOpcion(p_opciones:string){
     this._opciones.push(p_opciones);
@@ -19,7 +21,12 @@ public setOpcionCorrecta(p_opcionCorrecta:number){
     this._opcionCorrecta=p_opcionCorrecta;
 }
 public esCorrecta(p_respuesta:number):boolean{
-    return false
+    if (this._opcionCorrecta = p_respuesta){
+        return true;
+    } else {
+        return false
+    }
+    
 }
 public getPuntaje():number{
     return this._puntaje;
