f9dca5d69b88c95eefbd9bec88ee813b28332e32
A90G <104040612+A90G@users.noreply.github.com>
2022-11-02 15:37:16 -0300

    Update class_Pregunta.ts
    -

--- class/class_Pregunta.ts
+++ class/class_Pregunta.ts
@@ -12,20 +12,20 @@ constructor (p_consigna:string, p_opciones:Array<string>,p_opcionCorrecta:number
  public pregunta(p_consigna:string, p_puntaje:number){
     this._consigna = p_consigna;
     this._puntaje = p_puntaje;
-    console.log("pregunta: "+this._consigna+" Con Puntaje de: "+this._puntaje)
- }
+    console.log("pregunta: "this._consigna+" Con Puntaje de: "+this._puntaje);
+}
  public addOpcion(p_opciones:string){
     this._opciones.push(p_opciones);
- }
-public setOpcionCorrecta(p_opcionCorrecta:number){
-    this._opcionCorrecta = p_opcionCorrecta;
-    console.log("La Opcion correcta es :"+this._opcionCorrecta)
 }
-public esCorrecta(p_respuesta:number):boolean{
+public setOpcionCorrecta(p_opcionCorrecta:number)  {
+    this._opcionCorrecta = p_opcionCorrecta;
+    console.log("La Opcion correcta es :"+this._opcionCorrecta);
+}
+public esCorrecta(p_respuesta:number): boolean {
     if (this._opcionCorrecta = p_respuesta){
         return true;
     } else {
-        return false
+        return false;
     }
 }
 public getPuntaje():number{
